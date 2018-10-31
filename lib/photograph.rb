5c53c835bc6b532383189204b0117da1d15a4c77
Aizar <aizar.cabrera@icloud.com>
2018-10-31 09:22:16 -0600

    adds second test passing

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,6 +1,14 @@
 require 'pry'
 
 class Photograph
+
+  attr_reader :id,:name, :artist_id, :year
+
   def initialize(attributes)
+    @id        = attributes[:id].to_i
+    @name      = attributes[:name]
+    @artist_id = attributes[:artist_id].to_i
+    @year      = attributes[:year]
   end
+
 end
