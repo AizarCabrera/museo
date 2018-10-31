745ad55077db6f0920eff516fe99e26e440953b0
Aizar <aizar.cabrera@icloud.com>
2018-10-31 09:31:52 -0600

    adds second test passing for artist

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,13 @@ require 'pry'
 
 class Artist
 
-  def initialize(atributes)
+  attr_reader :id,:name, :born, :died, :country
+
+  def initialize(artist_atributes)
+    @id = artist_atributes[:id]
+    @name = artist_atributes[:name]
+    @born = artist_atributes[:born]
+    @died = artist_atributes[:died]
+    @country = artist_atributes[:country]
   end
 end
--- test/artist_test.rb
+++ test/artist_test.rb
@@ -17,27 +17,19 @@ class ArtistTest < Minitest::Test
       assert_instance_of Artist, artist
     end
 
+    def test_it_has_attributes
+      attributes = {
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+      }
+      artist = Artist.new(attributes)
+      assert_equal "2", artist.id
+      assert_equal "Ansel Adams", artist.name
+      assert_equal "1902",artist.born
+      assert_equal "1984", artist.died
+      assert_equal "United States", artist.country
+    end
 end
-
-
-
-
-# pry(main)>
-#
-# pry(main)>
-# #=> #<Artist:0x00007fc2d0a6c080...>
-#
-# pry(main)> artist.id
-# #=> "2"
-#
-# pry(main)> artist.name
-# #=> "Ansel Adams"
-#
-# pry(main)> artist.born
-# #=> "1902"
-#
-# pry(main)> artist.died
-# #=> "1984"
-#
-# pry(main)> artist.country
-# #=> "United States"
